0457ba361363b22687d3eb1a0242cdd177280c80
Warren Moore <wm@warrenmoore.net>
2022-04-30 14:27:19 -0700

    Added sample code for day 21

---
+++ 21/MetalPointLights/MetalPointLights/AppDelegate.swift
@@ -0,0 +1,11 @@
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+
---
+++ 21/MetalPointLights/MetalPointLights/Renderer.swift
@@ -0,0 +1,363 @@
+
+import Cocoa
+import Metal
+import MetalKit
+import simd
+
+let MaxOutstandingFrameCount = 3
+let MaxConstantsSize = 1_024 * 1_024
+let MinBufferAlignment = 256
+
+struct NodeConstants {
+    var modelMatrix: float4x4
+}
+
+struct FrameConstants {
+    var projectionMatrix: float4x4
+    var viewMatrix: float4x4
+    var lightCount: UInt32
+}
+
+struct LightConstants {
+    var viewProjectionMatrix: simd_float4x4
+    var intensity: simd_float3
+    var position: simd_float3
+    var direction: simd_float3
+    var type: UInt32
+}
+
+struct ShadowConstants {
+    var modelViewProjectionMatrix: simd_float4x4
+}
+
+class Renderer: NSObject, MTKViewDelegate {
+    let device: MTLDevice
+    let commandQueue: MTLCommandQueue
+    let view: MTKView
+
+    var vertexDescriptor: MTLVertexDescriptor!
+
+    var nodes = [Node]()
+    var lights = [Light]()
+
+    var cowNode: Node!
+    var floorNode: Node!
+    var orbitLights = [Light]()
+
+    private var constantBuffer: MTLBuffer!
+    private var currentConstantBufferOffset = 0
+    private var frameConstantsOffset = 0
+    private var lightConstantsOffset = 0
+    private var nodeConstantsOffsets = [Int]()
+
+    private var renderPipelineState: MTLRenderPipelineState!
+    private var shadowRenderPipelineState: MTLRenderPipelineState!
+
+    private var depthStencilState: MTLDepthStencilState!
+    private var samplerState: MTLSamplerState!
+
+    private var frameSemaphore = DispatchSemaphore(value: MaxOutstandingFrameCount)
+    private var frameIndex = 0
+    private var time: TimeInterval = 0
+
+    init(device: MTLDevice, view: MTKView) {
+        self.device = device
+        self.commandQueue = device.makeCommandQueue()!
+        self.view = view
+
+        super.init()
+
+        view.device = device
+        view.delegate = self
+        view.colorPixelFormat = .bgra8Unorm_srgb
+        view.depthStencilPixelFormat = .depth32Float
+        view.clearColor = MTLClearColor(red: 0.0, green: 0.5, blue: 0.95, alpha: 1.0)
+        view.sampleCount = 4
+
+        makeScene()
+        makeResources()
+        makePipelines()
+    }
+
+    func makeScene() {
+        let allocator = MTKMeshBufferAllocator(device: device)
+
+        let mdlVertexDescriptor = MDLVertexDescriptor()
+        mdlVertexDescriptor.vertexAttributes[0].name = MDLVertexAttributePosition
+        mdlVertexDescriptor.vertexAttributes[0].format = .float3
+        mdlVertexDescriptor.vertexAttributes[0].offset = 0
+        mdlVertexDescriptor.vertexAttributes[0].bufferIndex = 0
+        mdlVertexDescriptor.vertexAttributes[1].name = MDLVertexAttributeNormal
+        mdlVertexDescriptor.vertexAttributes[1].format = .float3
+        mdlVertexDescriptor.vertexAttributes[1].offset = 12
+        mdlVertexDescriptor.vertexAttributes[1].bufferIndex = 0
+        mdlVertexDescriptor.vertexAttributes[2].name = MDLVertexAttributeTextureCoordinate
+        mdlVertexDescriptor.vertexAttributes[2].format = .float2
+        mdlVertexDescriptor.vertexAttributes[2].offset = 24
+        mdlVertexDescriptor.vertexAttributes[2].bufferIndex = 0
+        mdlVertexDescriptor.bufferLayouts[0].stride = 32
+
+        vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlVertexDescriptor)!
+
+        let assetURL = Bundle.main.url(forResource: "spot_triangulated", withExtension: "obj")
+        let mdlAsset = MDLAsset(url: assetURL, vertexDescriptor: mdlVertexDescriptor, bufferAllocator: allocator)
+
+        mdlAsset.loadTextures()
+
+        let meshes = mdlAsset.childObjects(of: MDLMesh.self) as? [MDLMesh]
+        guard let mdlMesh = meshes?.first else {
+            fatalError("Did not find any meshes in the Model I/O asset")
+        }
+
+        let textureLoader = MTKTextureLoader(device: device)
+        let options: [MTKTextureLoader.Option : Any] = [
+            .textureUsage : MTLTextureUsage.shaderRead.rawValue,
+            .textureStorageMode : MTLStorageMode.private.rawValue,
+            .origin : MTKTextureLoader.Origin.bottomLeft.rawValue
+        ]
+
+        var texture: MTLTexture?
+        let firstSubmesh = mdlMesh.submeshes?.firstObject as? MDLSubmesh
+        let material = firstSubmesh?.material
+        if let baseColorProperty = material?.property(with: MDLMaterialSemantic.baseColor) {
+            if baseColorProperty.type == .texture, let textureURL = baseColorProperty.urlValue {
+                texture = try? textureLoader.newTexture(URL: textureURL, options: options)
+            }
+        }
+
+        let mesh = try! MTKMesh(mesh: mdlMesh, device: device)
+
+        cowNode = Node(mesh: mesh)
+        cowNode.texture = texture
+
+        let floorTextureURL = Bundle.main.url(forResource: "grass", withExtension: "png")!
+        texture = try? textureLoader.newTexture(URL: floorTextureURL, options: options)
+
+        let mdlPlane = MDLMesh(planeWithExtent: SIMD3<Float>(4, 0, 4),
+                               segments: SIMD2<UInt32>(1, 1),
+                               geometryType: .triangles,
+                               allocator: allocator)
+        let mtkPlane = try! MTKMesh(mesh: mdlPlane, device: device)
+
+        floorNode = Node(mesh: mtkPlane)
+        floorNode.texture = texture
+
+        nodes = [floorNode, cowNode]
+
+        let ambientLight = Light()
+        ambientLight.type = .ambient
+        ambientLight.intensity = 0.1
+
+        let orbitLight0 = Light()
+        orbitLight0.color = SIMD3<Float>(0.45, 0, 0.95)
+        orbitLight0.intensity = 0.8
+        orbitLight0.type = .omni
+
+        let orbitLight1 = Light()
+        orbitLight1.color = SIMD3<Float>(0.95, 0.45, 0)
+        orbitLight1.intensity = 0.8
+        orbitLight1.type = .omni
+
+        let orbitLight2 = Light()
+        orbitLight2.color = SIMD3<Float>(0, 0.95, 0.45)
+        orbitLight2.intensity = 0.8
+        orbitLight2.type = .omni
+
+        orbitLights = [orbitLight0, orbitLight1, orbitLight2]
+
+        lights = [ambientLight] + orbitLights
+    }
+
+    func makeResources() {
+        constantBuffer = device.makeBuffer(length: MaxConstantsSize,
+                                           options: .storageModeShared)
+        constantBuffer.label = "Dynamic Constant Buffer"
+    }
+
+    func makePipelines() {
+        guard let library = device.makeDefaultLibrary() else {
+            fatalError("Unable to create default Metal library")
+        }
+
+        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        renderPipelineDescriptor.rasterSampleCount = view.sampleCount
+
+        renderPipelineDescriptor.vertexDescriptor = vertexDescriptor
+
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat
+        renderPipelineDescriptor.depthAttachmentPixelFormat = view.depthStencilPixelFormat
+
+        renderPipelineDescriptor.vertexFunction = library.makeFunction(name: "vertex_main")!
+        renderPipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragment_main")!
+
+        do {
+            renderPipelineState = try device.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        } catch {
+            fatalError("Error while creating render pipeline state: \(error)")
+        }
+
+        let depthStencilDescriptor = MTLDepthStencilDescriptor()
+        depthStencilDescriptor.depthCompareFunction = .less
+        depthStencilDescriptor.isDepthWriteEnabled = true
+        depthStencilState = device.makeDepthStencilState(descriptor: depthStencilDescriptor)!
+
+        let samplerDescriptor = MTLSamplerDescriptor()
+        samplerDescriptor.normalizedCoordinates = true
+        samplerDescriptor.magFilter = .linear
+        samplerDescriptor.minFilter = .linear
+        samplerDescriptor.mipFilter = .linear
+        samplerDescriptor.sAddressMode = .repeat
+        samplerDescriptor.tAddressMode = .repeat
+        samplerState = device.makeSamplerState(descriptor: samplerDescriptor)!
+    }
+
+    func allocateConstantStorage(size: Int, alignment: Int) -> Int {
+        let effectiveAlignment = lcm(alignment, MinBufferAlignment)
+        var allocationOffset = align(currentConstantBufferOffset, upTo: effectiveAlignment)
+        if (allocationOffset + size >= MaxConstantsSize) {
+            allocationOffset = 0
+        }
+        currentConstantBufferOffset = allocationOffset + size
+        return allocationOffset
+    }
+
+    func updateFrameConstants() {
+        time += (1.0 / Double(view.preferredFramesPerSecond))
+
+        let cameraPosition = SIMD3<Float>(0, 0.5, 2)
+        let viewMatrix = simd_float4x4(translate: -cameraPosition)
+
+        let aspectRatio = Float(view.drawableSize.width / view.drawableSize.height)
+        let projectionMatrix = simd_float4x4(perspectiveProjectionFoVY: .pi / 3,
+                                             aspectRatio: aspectRatio,
+                                             near: 0.01,
+                                             far: 100)
+
+        var constants = FrameConstants(projectionMatrix: projectionMatrix,
+                                       viewMatrix: viewMatrix,
+                                       lightCount: UInt32(lights.count))
+
+        let layout = MemoryLayout<FrameConstants>.self
+        frameConstantsOffset = allocateConstantStorage(size: layout.size, alignment: layout.stride)
+        let constantsPointer = constantBuffer.contents().advanced(by: frameConstantsOffset)
+        constantsPointer.copyMemory(from: &constants, byteCount: layout.size)
+    }
+
+    func updateLightConstants() {
+        let t = Float(time)
+
+        let orbitRadius: Float = 1.0
+        for i in 0..<orbitLights.count {
+            let lightAngleOffset = ((2 * Float.pi) / Float(orbitLights.count)) * Float(i)
+            let lightAngle = t + lightAngleOffset
+
+            let orbitLightTransform = simd_float4x4(translate: SIMD3<Float>(orbitRadius * cos(lightAngle),
+                                                                            1.0,
+                                                                            orbitRadius * sin(lightAngle)))
+            orbitLights[i].worldTransform = orbitLightTransform
+        }
+
+        let layout = MemoryLayout<LightConstants>.self
+        lightConstantsOffset = allocateConstantStorage(size: layout.stride * lights.count, alignment: layout.stride)
+        let lightsBufferPointer = constantBuffer.contents().advanced(by: lightConstantsOffset).assumingMemoryBound(to: LightConstants.self)
+
+        for (lightIndex, light) in lights.enumerated() {
+            let shadowViewMatrix = light.worldTransform.inverse
+            let shadowProjectionMatrix = light.projectionMatrix
+            let shadowViewProjectionMatrix = shadowProjectionMatrix * shadowViewMatrix
+
+            lightsBufferPointer[lightIndex] = LightConstants(viewProjectionMatrix: shadowViewProjectionMatrix,
+                                                             intensity: light.color * light.intensity,
+                                                             position: light.position,
+                                                             direction: light.direction,
+                                                             type: light.type.rawValue)
+        }
+    }
+
+    func updateNodeConstants() {
+        let rotationAxis = normalize(SIMD3<Float>(0, 1, 0))
+        let rotation = simd_float4x4(rotateAbout: rotationAxis, byAngle: 270.0 * (180.0 / .pi))
+
+        cowNode.transform = rotation
+
+        nodeConstantsOffsets.removeAll()
+        for node in nodes {
+            var constants = NodeConstants(modelMatrix: node.worldTransform)
+
+            let layout = MemoryLayout<NodeConstants>.self
+            let offset = allocateConstantStorage(size: layout.size, alignment: layout.stride)
+            let constantsPointer = constantBuffer.contents().advanced(by: offset)
+            constantsPointer.copyMemory(from: &constants, byteCount: layout.size)
+            nodeConstantsOffsets.append(offset)
+        }
+    }
+
+    func drawMainPass(renderPassDescriptor: MTLRenderPassDescriptor, commandBuffer: MTLCommandBuffer) {
+        let renderCommandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+        renderCommandEncoder.setRenderPipelineState(renderPipelineState)
+
+        renderCommandEncoder.setDepthStencilState(depthStencilState)
+        renderCommandEncoder.setFrontFacing(.counterClockwise)
+        renderCommandEncoder.setCullMode(.back)
+
+        renderCommandEncoder.setVertexBuffer(constantBuffer, offset: 0, index: 2)
+        renderCommandEncoder.setVertexBuffer(constantBuffer, offset: frameConstantsOffset, index: 3)
+        renderCommandEncoder.setFragmentBuffer(constantBuffer, offset: frameConstantsOffset, index: 3)
+        renderCommandEncoder.setFragmentBuffer(constantBuffer, offset: lightConstantsOffset, index: 4)
+
+        for (objectIndex, node) in nodes.enumerated() {
+            guard let mesh = node.mesh else { continue }
+
+            renderCommandEncoder.setVertexBufferOffset(nodeConstantsOffsets[objectIndex], index: 2)
+
+            for (i, meshBuffer) in mesh.vertexBuffers.enumerated() {
+                renderCommandEncoder.setVertexBuffer(meshBuffer.buffer,
+                                                     offset: meshBuffer.offset,
+                                                     index: i)
+            }
+
+            renderCommandEncoder.setFragmentTexture(node.texture, index: 0)
+            renderCommandEncoder.setFragmentSamplerState(samplerState, index: 0)
+
+            for submesh in mesh.submeshes {
+                let indexBuffer = submesh.indexBuffer
+                renderCommandEncoder.drawIndexedPrimitives(type: submesh.primitiveType,
+                                                           indexCount: submesh.indexCount,
+                                                           indexType: submesh.indexType,
+                                                           indexBuffer: indexBuffer.buffer,
+                                                           indexBufferOffset: indexBuffer.offset)
+            }
+        }
+
+        renderCommandEncoder.endEncoding()
+    }
+
+    // MARK: - MTKViewDelegate
+
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+
+    func draw(in view: MTKView) {
+        frameSemaphore.wait()
+
+        updateLightConstants()
+        updateFrameConstants()
+        updateNodeConstants()
+
+        guard let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+
+        guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
+
+        drawMainPass(renderPassDescriptor: renderPassDescriptor, commandBuffer: commandBuffer)
+
+        commandBuffer.present(view.currentDrawable!)
+
+        commandBuffer.addCompletedHandler { [weak self] _ in
+            self?.frameSemaphore.signal()
+        }
+
+        commandBuffer.commit()
+
+        frameIndex += 1
+    }
+}
