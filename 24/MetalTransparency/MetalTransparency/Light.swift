380cbc3588ad854ee86306c01f874fe400cbc4b5
Warren Moore <wm@warrenmoore.net>
2022-05-04 15:12:35 -0700

    Added sample code for day 24

---
+++ 24/MetalTransparency/MetalTransparency/Light.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+import Metal
+import simd
+
+enum LightType : UInt32 {
+    case ambient
+    case directional
+    case omni
+}
+
+class Light {
+    var type = LightType.directional
+    var color = SIMD3<Float>(1, 1, 1)
+    var intensity: Float = 1.0
+
+    var worldTransform: simd_float4x4 = matrix_identity_float4x4
+
+    var position: SIMD3<Float> {
+        return worldTransform.columns.3.xyz
+    }
+
+    var direction: SIMD3<Float> {
+        return -worldTransform.columns.2.xyz
+    }
+
+    var castsShadows = false
+    var shadowTexture: MTLTexture?
+
+    var projectionMatrix: simd_float4x4 {
+        return simd_float4x4(orthographicProjectionWithLeft: -1.5,
+                             top: 1.5,
+                             right: 1.5,
+                             bottom: -1.5,
+                             near: 0,
+                             far: 10)
+    }
+}
